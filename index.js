b79e479f7fb97bad392c116c06e76e5d78ec233b
juanitaHoyos <juanitahoyos1@gmail.com>
2019-05-07 19:05:06 -0500

    se hace primer filtro

--- index.js
+++ index.js
@@ -5,29 +5,20 @@ var app= express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 const url = 'mongodb://localhost:27017';
+
 const dbname = 'albatroz';
-const client = new MongoClient(url);
+
+let db;
+
+const client = new MongoClient(url, { useNewUrlParser: true });
+
 client.connect(function(err){
-
+    
     assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbname);
-
-  const productos = db.collection('productos');
-  productos.find({}, {sort:['precio']}).toArray(function(err,docs){
-    assert.equal(null,err);
-    console.log('encontramos los docs');
-   docs.forEach(function(prod){
-
-    console.log(prod.precio);
-   });
-
-
-  });
-
-  client.close();
-
+    console.log("Connected successfully to server");
+    
+    db = client.db(dbname);
+    
 });
 
 var motorRender = require('express-handlebars');
@@ -44,18 +35,33 @@ let productos = JSON.parse(fileReaded);
 
 
 app.get('/', function(req, res){
-
+    
     res.render('home');
     
 });
 
 app.get('/store', function(req,res){
-    var contexto = {
-        listaProductos: productos
-    };
-    res.render('store', contexto);
+    
+    var query = {};
+    
+    if(req.query.dias){
+        query.dias = parseInt(req.query.dias);
+    }
+    
+    const productos = db.collection('productos');
+    
+    productos.find(query,{}).toArray(function(err, docs){
+        assert.equal(null,err);
+        console.log('encontramos los docs');
+        
+        var contexto = {
+            listaProductos: docs
+        };
+        res.render('store', contexto);        
+    });
+    
 });
 
 app.listen(3000,function(){
-console.log('hola!');
+    console.log('hola!');
 });
