bdd54af8c5995e1263138a899428595ab268d7a1
juanitaHoyos <juanitahoyos1@gmail.com>
2019-05-08 21:01:22 -0500

    banner en producto particular

--- public/js/store.js
+++ public/js/store.js
@@ -10,3 +10,11 @@ if(intPrecio){
     intPrecio.addEventListener('change', buscarPorPrecio);
 }
 
+var titulos = document.querySelectorAll(".producto__nombre");
+
+titulos.forEach(titulo => {
+   // console.log(titulo.innerHTML);
+ var  str= titulo.innerHTML;
+titulo.innerHTML=str.toUpperCase();
+    
+});
